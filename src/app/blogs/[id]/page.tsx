729e8f0bd1a69d9da4a23b2c4a1ebfc17b2d3ea8
naimatullahRezaee <naimatullahrezaee@gmail.com>
2025-09-21 08:02:23 +0430

    add details blog page

--- src/app/blogs/[id]/page.tsx
+++ src/app/blogs/[id]/page.tsx
@@ -1,7 +1,42 @@
-import React from "react";
+import Container from "@/components/Container";
+import { IGetArticles } from "../page";
+import Link from "next/link";
 
-function Article() {
-  return <div>Article</div>;
+interface IArticleProps {
+  params: Promise<{ id: string }>;
+  searchParams: Promise<{}>;
+}
+
+async function Article(props: IArticleProps) {
+  const { id } = await props.params;
+  const result = await fetch(`http://localhost:3001/articles/${id}`);
+  const data = (await result.json()) as IGetArticles;
+  return (
+    <div>
+      <Container>
+        <Link href={"/blogs"}>
+          <div className="max-w-4xl mx-auto px-4 py-12">
+            <div className="bg-white rounded-2xl shadow-lg overflow-hidden">
+              <img
+                src={data.image}
+                alt={data.title}
+                className="w-full h-80 object-cover"
+              />
+
+              <div className="p-6 sm:p-10">
+                <h1 className="text-3xl sm:text-4xl font-bold text-gray-800 mb-4">
+                  {data.title}
+                </h1>
+                <p className="text-gray-600 leading-relaxed text-lg">
+                  {data.description}
+                </p>
+              </div>
+            </div>
+          </div>
+        </Link>
+      </Container>
+    </div>
+  );
 }
 
 export default Article;
--- src/app/blogs/page.tsx
+++ src/app/blogs/page.tsx
@@ -1,6 +1,8 @@
 import React from "react";
 import Container from "../../components/Container";
 import Articles from "@/components/Articles";
+import Link from "next/link";
+import { it } from "node:test";
 
 export interface IGetArticles {
   id: number;
@@ -16,7 +18,9 @@ async function Blogs() {
     <Container>
       <div className="grid grid-cols-4 gap-4 py-12">
         {data.map((item) => (
-          <Articles key={item.id} {...item} />
+          <Link key={item.id} href={`/blogs/${item.id}`}>
+            <Articles {...item} />
+          </Link>
         ))}
       </div>
     </Container>
